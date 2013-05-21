7062f85c3bb5aaffc8d1a785d343c918a6787729
Todd Rinaldo <toddr@cpan.org>
2013-05-20 23:10:49 -0500

    Bump to version 1.27

--- lib/YAML/Syck.pm
+++ lib/YAML/Syck.pm
@@ -14,7 +14,7 @@ use 5.006;
 use Exporter;
 
 BEGIN {
-    $VERSION   = '1.26';
+    $VERSION   = '1.27';
     @EXPORT    = qw( Dump Load DumpFile LoadFile );
     @EXPORT_OK = qw( DumpInto );
     @ISA       = qw( Exporter );
